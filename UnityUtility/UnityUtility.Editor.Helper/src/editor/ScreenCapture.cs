62193289c64b32cf7823bc3dcc4ba449fe2eab0a
ade <ade.li.indie@gmail.com>
2018-01-27 12:46:39 +0800

    Init

---
+++ UnityUtility/UnityUtility.Editor.Helper/src/editor/ScreenCapture.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+namespace UnityUtility.Editor.Helper
+{
+    public class EditorHelper : MonoBehaviour {
+
+        [MenuItem("Tools/Utility/Screenshot/TakeScreenshot")]
+        private static void Screenshot() {
+            string filename = string.Format ("/Screenshot_{0}.png", Now());
+            string path = Application.persistentDataPath + filename;
+            Debug.Log ("Screenshot: " + path);
+            ScreenCapture.CaptureScreenshot (path);
+        }
+
+        static int Now() {
+            return (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        }
+    }
+}
+
